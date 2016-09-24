3f858315da97561f8a5442c39e384c9d8b25f28a
dineshmadhup <dineshmadhup@gmail.com>
2016-09-24 12:56:23 -0700

    File name changed from module2.html to index.html

--- Module-2-shoppingList/app.js
+++ Module-2-shoppingList/app.js
@@ -1,50 +1,75 @@
+(function () {
+    'use strict';
 
-(function(){
-   'use strict';
-angular.module('app', [])
-    .controller('MoveCtrl', function($scope) {
-           
-    $scope.moveItem = function(items, from, to) {
+//Modules
+    angular.module('ShoppingListCheckoff', [])
+        .controller('ToBuyShoppingController', ToBuyShoppingController)
+        .controller('AlreadyBoughtShoppingController', AlreadyBoughtShoppingController)
+        .service('ShoppingListCheckOffService', ShoppingListCheckOffService);
 
-        items.forEach(function(item) {
-          var idx = from.indexOf(item);
-          if (idx != -1) {
-              from.splice(idx, 1);
-              to.push(item);      
-          }
-        });
-    };
-    $scope.moveAll = function(from, to) {
 
-        angular.forEach(from, function(item) {
-            to.push(item);
-        });
-        from.length = 0;
-    };                
+//This line for minification purposes
+    ToBuyShoppingController.$inject = ['ShoppingListCheckOffService'];
+//Function controller
+    function ToBuyShoppingController(ShoppingListCheckOffService) {
+        var toBuy = this;
 
-    $scope.selecteditems = [];                                
+        toBuy.items = ShoppingListCheckOffService.toBuyItems();
 
-    $scope.availableitems = [
-      {
-        id: 1, 
-        name: 'Apple cookie-10 box'
-      }, 
-      {
-        id: 2, 
-        name: 'Berger Cookie-5 box'
-      },
-      {
-        id: 3,
-        name: 'Biscuit-3box'
-      },
-        {
-        id: 4,
-        name: 'Boortsog-2box'
-      },
-        {
-        id: 5,
-        name: 'Bredela-1box'
-      }
-    ];
-  }); 
+        toBuy.buyToBought = function (itemIndex) {
+            ShoppingListCheckOffService.buyToBought(itemIndex);
+        }
+    }
+
+//This line for minification purposes
+    AlreadyBoughtShoppingController.$inject = ['ShoppingListCheckOffService'];
+//Function controller
+    function AlreadyBoughtShoppingController(ShoppingListCheckOffService) {
+        var alreadyBought = this;
+
+        alreadyBought.items = ShoppingListCheckOffService.boughtItems();
+    }
+
+//Services    
+    function ShoppingListCheckOffService() {
+        var service = this;
+        var toBuy = [];
+        var alreadyBought = [];
+
+//Function to move items from to buy to already bought
+        service.buyToBought = function (itemIndex) {
+            alreadyBought.push(toBuy[itemIndex]); //add to bought list
+            toBuy.splice(itemIndex, 1); //remove from to buy
+        }
+        service.addItem = function (itemName, itemQuantity) {
+            var item = {
+                name: itemName,
+                quantity: itemQuantity
+            }
+            toBuy.push(item);
+        }
+
+        //To Buy Items
+        var totalItems = [
+            {"name": "Choclate chips", "quantity": 10},
+            {"name": "Oatmeal", "quantity": 50},
+            {"name": "Apple", "quantity": 30},
+            {"name": "Biscuit", "quantity": 11},
+            {"name": "Banana Nuts", "quantity": 40}
+        ];
+
+        for (var i = 0; i < totalItems.length; i++) {
+            service.addItem(totalItems[i].name, totalItems[i].quantity);
+        }
+
+        service.toBuyItems = function () {
+            return toBuy;
+        }
+
+        service.boughtItems = function () {
+            return alreadyBought;
+        }
+    }
+
 })();
+
